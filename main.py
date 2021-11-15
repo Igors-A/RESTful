3bab4e0e665b69a6bc2cab3bba24c9953b38c0e7
KrievsnoLatvijas <fy18ia@gmail.com>
2021-11-15 17:14:30 +0000

    added lines to server No. 2 and test flask as client

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from flask import Flask
 from flask_restful import Resource, Api
-
+import requests
 app = Flask(__name__)
 api = Api(app)
-
-class HelloWorld(Resource):
-    def get(self):
-        return "XUY"
-
-api.add_resource(HelloWorld, '/')
-
+@app.route("/")
+def jsontest():
+    jsn = requests.get('http://127.0.0.1:5000/sent/Counter-Strike').json()
+    return jsn
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=6268)
--- server2.py
+++ server2.py
@@ -1,17 +1,29 @@
 from flask import Flask
-from flask_restful import Resource, Api
-#import requests
-from google_images_search import GoogleImagesSearch
+from flask_restful import reqparse, abort, Api, Resource
+import sqlite3, json
 app = Flask(__name__)
 api = Api(app)
-
-#old key: AIzaSyDXQh87f9oCrYnzgwe4rOHAj7daN0z1Ef0
-#new key: AIzaSyBqGRyWj03v13mprwwVCh_XIbKa_c7MOB4
-#cx: 915e27a2c2a3f48ff
-gis = GoogleImagesSearch('AIzaSyBqGRyWj03v13mprwwVCh_XIbKa_c7MOB4', '915e27a2c2a3f48ff')
-sp={
-    'q':'...'
-}#search parameters
-gis.search(sp)
+con = sqlite3.connect('sources/steam1.db', check_same_thread=False)
+cur = con.cursor()
+cur.execute("SELECT * FROM games LIMIT 1")
+top = cur.fetchall()[0]
+#print(top)
+class Agame():
+    def __init__(self,game):
+        #print(type(game))
+        cur.execute("SELECT * FROM games WHERE column2="+"\'"+game+"\'")
+        arr = cur.fetchall()[0]
+        x = {}
+        for i in range(len(top)-7):
+            x.update({top[i]:arr[i]})
+        self.jsn = json.dumps(x)
+#a = Agame('Counter-Strike')
+#print(a.jsn)
+class Sender(Resource):
+    def get(self, game):
+        self.a = Agame(game)
+        #print(self.a.jsn)
+        return self.a.jsn
+api.add_resource(Sender, '/sent/<game>')
 if __name__ == '__main__':
     app.run(debug=True)
