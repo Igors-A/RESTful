c1dd78579666609fcc80c16be0a1a29d59f970f0
KrievsnoLatvijas <fy18ia@gmail.com>
2021-11-16 04:47:29 +0000

    finished server2.py, I think

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Resource, Api
 import requests
+#test client by Igors Anastasjevs
 app = Flask(__name__)
 api = Api(app)
 @app.route("/")
--- server2.py
+++ server2.py
@@ -1,29 +1,58 @@
+#done by Igors Annastasjevs, fy18ia
+
+
+import random
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 import sqlite3, json
-app = Flask(__name__)
+
+app = Flask(__name__)#set ups Flask-based server
 api = Api(app)
-con = sqlite3.connect('sources/steam1.db', check_same_thread=False)
+con = sqlite3.connect('sources/steam1.db', check_same_thread=False)#connects to database
 cur = con.cursor()
 cur.execute("SELECT * FROM games LIMIT 1")
-top = cur.fetchall()[0]
-#print(top)
-class Agame():
+top = cur.fetchall()[0]#get keys for json/dictionary in order to sort information about the game from steam1.db
+#steam1.db was created from steam.csv, which can be openly received from https://www.kaggle.com/nikdavis/steam-store-games?select=steam.csv
+#steam1.db was slightly modified adding with appr. 5 games
+def fromtupletodicofdb(t):#function which basically explaining creates dictionary from tuple
+    thegame = {}
+    for i in range(len(top) - 7):#columms from 12 to 18 were excess
+        thegame.update({top[i]: t[i]})
+    return thegame
+class Agame():#a game class, its "legacy" class as it was repurposed from storing 1 game into storing 1 game and 3 recommendations
     def __init__(self,game):
-        #print(type(game))
-        cur.execute("SELECT * FROM games WHERE column2="+"\'"+game+"\'")
-        arr = cur.fetchall()[0]
-        x = {}
-        for i in range(len(top)-7):
-            x.update({top[i]:arr[i]})
-        self.jsn = json.dumps(x)
-#a = Agame('Counter-Strike')
-#print(a.jsn)
+        cur.execute("SELECT * FROM games WHERE column2="+"\'"+game+"\'")#searches a game
+        out = cur.fetchall()
+        thegame = fromtupletodicofdb(out[0])
+        genres = thegame['genres'].split(';')#for recommendation getting
+        #коммент ниже можно удалить
+        """this was added because one of the games had this tag. The game, which names I tested, had 1% of 'Sexual Content',
+        but it was enough for sqlite to present a lot of possibly pornographic games"""
+        #command generation
+        if 'Sexual Content' in genres:
+            genres.remove('Sexual Content')
+        command = "SELECT * FROM games WHERE (column10 LIKE \'%"+genres[0]+"%\'"
+        if(len(genres)>1):
+            for i in range(1,len(genres)):
+                command += " OR \'%" + genres[i] + "%\'"
+        command += ") AND ( NOT column2="+"\'"+game+"\')"
+        cur.execute(command)
+        #receiving data
+        out = cur.fetchall()
+        arr = []
+        #random recommentations from array
+        for i in range(0,3):
+            j = random.randint(0, len(out))
+            arr.append(fromtupletodicofdb(out[j]))
+        getout = {
+            "the game": thegame,#the game receive from client
+            "recomm": arr#array of recommended games
+        }
+        self.jsn = json.dumps(getout)#sents 'json'
 class Sender(Resource):
     def get(self, game):
-        self.a = Agame(game)
-        #print(self.a.jsn)
-        return self.a.jsn
+        self.a = Agame(game)#getting data
+        return self.a.jsn#sending data
 api.add_resource(Sender, '/sent/<game>')
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)#ports could be changed here in case of
